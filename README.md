a7e31db2f7b5c2612bb0a28a54894f13fc367bfc
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-12-24 10:52:01 +0100

    added sync functionality

--- README.md
+++ README.md
@@ -30,12 +30,24 @@ The repository is compatible with the puppet naturalis/puppet-docker_compose man
 docker-compose up -d
 ````
 
+Backup / Restore
+-------------
+For rsync / database backup from restic functionality a restore_and_sync.sh script is available in the custom folder. This can be run interactive and depends on: 
+in general this will be configured as a post restore script in restic
+on the receiving server. 
+- database dump file in /var/backup/pgsql/pgsql_jiradb.dump
+- docker-compose.yml should be in /opt/composeproject
+- RSYNC_SOURCE variable in .env , the DNS name of the source/prod server
+- SSH passwordless key pair with private key in /opt/composeproject/.jirarsync.key 
+- logrotate for /var/log/jira/*.log
+on the source/ prod server
+- jirarsync user with public key in authorised_keys
+- data of jira in directory /data/jira/data and owner:group should be ubuntu:jirarsync
+
+
 Usage
 -------------
 
-
-
 If there is a valid traefik.toml with or without SSL then both services can be accessed through port 80/443. 
 It is advised to setup firewall rules and only allow 80/443 to the server running the docker-compose project, use port 8080,9000 and 8081 using a SSH tunnel.
-
 
--- custom/restore_and_sync.sh
+++ custom/restore_and_sync.sh
@@ -3,15 +3,43 @@
 # Restore and rsync script to be used as restic post restore script
 # assumes docker-compose.yml is available in /opt/composeproject
 #
+
+# enter docker-compose dir
 cd /opt/composeproject
+
+# source environment vars from .env 
 echo Source environment from .env
 export $(cat .env | xargs)
+
+# stop jira
 echo Stopping jira container to avoid access to the database. 
 docker-compose stop jira
+
+# move db file
+echo Move data to volume location in container
+mv /var/backup/pgsql/pgsql_jiradb.dump /data/postgresqldata/pgsql_jiradb.dump
+
+# import db
 echo Importing dumpfile
-docker-compose exec postgresql psql -f /var/backup/pgsql_jiradb.dump ${POSTGRES_DB} -U ${POSTGRES_USER} -P ${POSTGRES_PASSWORD} 
+docker-compose exec postgresql pg_restore /var/lib/postgresql/data/pgsql_jiradb.dump -c -d ${POSTGRES_DB} -U ${POSTGRES_USER} 
+
+# move db file back
+echo Move data to original location
+mv /data/postgresqldata/pgsql_jiradb.dump /var/backup/pgsql/pgsql_jiradb.dump
+
+# rsync data from production  Add ssh key if needed
+if ! grep "$(ssh-keyscan ${RSYNC_SOURCE} 2>/dev/null)" /root/.ssh/known_hosts > /dev/null; then
+    ssh-keyscan ${RSYNC_SOURCE} >> /root/.ssh/known_hosts
+fi
+
+# touch log and set permissions
+mkdir -p /var/log/jira
+touch /var/log/jira/rsync_data.log
+
+# rsync with --delete option, without compression. 
+rsync -avh -e 'ssh -i .jirarsync.key' --no-owner --no-group --delete --log-file=/var/log/jirar/rsync_data.log jirarsync@${RSYNC_SOURCE}:/data/jira/data /data/jira/data
+
+# start Jira
 echo Starting Jira
 docker-compose start jira
-
-
 
