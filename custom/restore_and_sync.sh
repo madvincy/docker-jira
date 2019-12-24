7ad169e2c73387b101eb8c5ec8f7bc80743d959d
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-12-24 11:17:01 +0100

    change to full path names for docker-compose, rsync and ssh-keyscan

--- custom/restore_and_sync.sh
+++ custom/restore_and_sync.sh
@@ -13,7 +13,7 @@ export $(cat .env | xargs)
 
 # stop jira
 echo Stopping jira container to avoid access to the database. 
-docker-compose stop jira
+/usr/local/bin/docker-compose stop jira
 
 # move db file
 echo Move data to volume location in container
@@ -21,7 +21,7 @@ mv /var/backup/pgsql/pgsql_jiradb.dump /data/postgresqldata/pgsql_jiradb.dump
 
 # import db
 echo Importing dumpfile
-docker-compose exec postgresql pg_restore /var/lib/postgresql/data/pgsql_jiradb.dump -c -d ${POSTGRES_DB} -U ${POSTGRES_USER} 
+/usr/local/bin/docker-compose exec postgresql pg_restore /var/lib/postgresql/data/pgsql_jiradb.dump -c -d ${POSTGRES_DB} -U ${POSTGRES_USER} 
 
 # move db file back
 echo Move data to original location
@@ -29,7 +29,7 @@ mv /data/postgresqldata/pgsql_jiradb.dump /var/backup/pgsql/pgsql_jiradb.dump
 
 # rsync data from production  Add ssh key if needed
 if ! grep "$(ssh-keyscan ${RSYNC_SOURCE} 2>/dev/null)" /root/.ssh/known_hosts > /dev/null; then
-    ssh-keyscan ${RSYNC_SOURCE} >> /root/.ssh/known_hosts
+    /usr/bin/ssh-keyscan ${RSYNC_SOURCE} >> /root/.ssh/known_hosts
 fi
 
 # touch log and set permissions
@@ -37,9 +37,9 @@ mkdir -p /var/log/jira
 touch /var/log/jira/rsync_data.log
 
 # rsync with --delete option, without compression. 
-rsync -avh -e 'ssh -i .jirarsync.key' --no-owner --no-group --delete --log-file=/var/log/jirar/rsync_data.log jirarsync@${RSYNC_SOURCE}:/data/jira/data /data/jira/data
+/usr/bin/rsync -avh -e 'ssh -i .jirarsync.key' --no-owner --no-group --delete --log-file=/var/log/jirar/rsync_data.log jirarsync@${RSYNC_SOURCE}:/data/jira/data /data/jira/data
 
 # start Jira
 echo Starting Jira
-docker-compose start jira
+/usr/local/bin/docker-compose start jira
 
